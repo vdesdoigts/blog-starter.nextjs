38ab7894ca9d108d28003d3f10e0462f58745e16
Vincent Desdoigts <v.desdoigts@gmail.com>
2024-02-10 19:31:32 +0100

    Use mdx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { Post, allPosts } from "contentlayer/generated";
 import { compareDesc, format, parseISO } from "date-fns";
+import { useMDXComponent } from "next-contentlayer/hooks";
 import Link from "next/link";
 
 function PostCard(post: Post) {
+  const MDXContent = useMDXComponent(post.body.code);
+
   return (
     <div className="mb-8">
       <h2 className="mb-1 text-xl">
@@ -16,10 +19,7 @@ function PostCard(post: Post) {
       <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
         {format(parseISO(post.date), "LLLL d, yyyy")}
       </time>
-      <div
-        className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
-        dangerouslySetInnerHTML={{ __html: post.body.html }}
-      />
+      <MDXContent />
     </div>
   );
 }
